a86fc10bffa83fd2274b2de8140cb86ed9dc0d9f
csa <csa@voxteneo.com>
2022-12-08 14:50:46 +0700

    practice css layouting dashboard

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
-const menu = document.querySelector(".menu")
-const menuBtn = document.querySelector(".menu-btn")
+const darkToggleIcon = document.querySelector("#darkToggle")
+const panel = document.querySelectorAll(".panel")
 
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('menu-open')
+darkToggleIcon.addEventListener('click', e => {
+    e.target.classList.toggle('fa-sun')
+    document.body.classList.toggle('dark-mode')
+})
+
+const r = document.querySelector(":root")
+const color = document.querySelectorAll(".colors li")
+
+color.forEach(c => {
+    c.addEventListener('click', () => {
+        const element = window.getComputedStyle(c)
+        let bg = element.getPropertyValue("background-color")
+        r.style.setProperty('--accent', bg)
+
+    })
 })
